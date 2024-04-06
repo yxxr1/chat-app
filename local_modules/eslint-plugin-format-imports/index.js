e1ea421288cb5515d5856ba43250c8baec0fc94c
yxxr1 <>
2024-04-06 19:41:52 +0300

    fix eslint plugin

--- local_modules/eslint-plugin-format-imports/index.js
+++ local_modules/eslint-plugin-format-imports/index.js
@@ -9,16 +9,19 @@ module.exports = {
         return {
           ImportDeclaration(node) {
             const nextToken = context.sourceCode.getTokenAfter(node);
-            const isImportNext = nextToken.type === 'Keyword' && nextToken.value === 'import';
 
-            if (!isImportNext && nextToken.loc.start.line - node.loc.end.line !== 2) {
-              context.report({
-                node,
-                message: '1 empty line after imports is required',
-                fix(fixer) {
-                  return fixer.replaceTextRange([node.range[1], nextToken.range[0]], '\n\n');
-                },
-              });
+            if (nextToken) {
+              const isImportNext = nextToken.type === 'Keyword' && nextToken.value === 'import';
+
+              if (!isImportNext && nextToken.loc.start.line - node.loc.end.line !== 2) {
+                context.report({
+                  node,
+                  message: '1 empty line after imports is required',
+                  fix(fixer) {
+                    return fixer.replaceTextRange([node.range[1], nextToken.range[0]], '\n\n');
+                  },
+                });
+              }
             }
           },
         };
