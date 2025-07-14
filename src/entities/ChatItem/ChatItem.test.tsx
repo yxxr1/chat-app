d346da92dd6447f3a7d3be4dd0f5de317ae1285e
yxxr1 <yr.7ce.rn@gmail.com>
2025-07-14 17:00:37 +0300

    test: Message

--- src/entities/ChatItem/ChatItem.test.tsx
+++ src/entities/ChatItem/ChatItem.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { cleanup, fireEvent, screen } from '@testing-library/react';
 import { ChatItem } from '@entities/ChatItem';
 import { Chat, Message } from '@store/types';
-import { renderComponent } from '../../shared/test/utils';
+import { renderComponent } from '@test/utils';
 import { THEMES } from '@styles/theme';
 import { MESSAGE_SERVICE_TYPES } from '@const/common';
 
@@ -29,12 +29,12 @@ describe('ChatItem renders', () => {
 
   test('renders content', () => {
     renderComponent(<ChatItem chat={chat} isCurrent={false} onClick={() => {}} />);
-    expect(screen.getByTestId('chatItem-container')).toMatchSnapshot();
+    expect(screen.getByTestId('chatItem__container')).toMatchSnapshot();
   });
 
   test('renders content no messages', () => {
     renderComponent(<ChatItem chat={{ ...chat, messages: [] }} isCurrent={false} onClick={() => {}} />);
-    expect(screen.getByTestId('chatItem-container')).toMatchSnapshot();
+    expect(screen.getByTestId('chatItem__container')).toMatchSnapshot();
   });
 
   test('renders content service message', () => {
@@ -45,12 +45,12 @@ describe('ChatItem renders', () => {
         onClick={() => {}}
       />,
     );
-    expect(screen.getByTestId('chatItem-container')).toMatchSnapshot();
+    expect(screen.getByTestId('chatItem__container')).toMatchSnapshot();
   });
 
   test('render isCurrent=false, theme light', () => {
     renderComponent(<ChatItem chat={chat} isCurrent={false} onClick={() => {}} />, 'light');
-    expect(screen.getByTestId('chatItem-container')).toHaveStyle({
+    expect(screen.getByTestId('chatItem__container')).toHaveStyle({
       'background-color': THEMES.light['background-alt'],
       border: `solid 1px ${THEMES.light.border}`,
     });
@@ -58,7 +58,7 @@ describe('ChatItem renders', () => {
 
   test('render isCurrent=true, theme light', () => {
     renderComponent(<ChatItem chat={chat} isCurrent={true} onClick={() => {}} />, 'light');
-    expect(screen.getByTestId('chatItem-container')).toHaveStyle({
+    expect(screen.getByTestId('chatItem__container')).toHaveStyle({
       'background-color': THEMES.light['background'],
       border: `solid 1px ${THEMES.light.primary}`,
     });
@@ -66,7 +66,7 @@ describe('ChatItem renders', () => {
 
   test('render isCurrent=false, theme dark', () => {
     renderComponent(<ChatItem chat={chat} isCurrent={false} onClick={() => {}} />, 'dark');
-    expect(screen.getByTestId('chatItem-container')).toHaveStyle({
+    expect(screen.getByTestId('chatItem__container')).toHaveStyle({
       'background-color': THEMES.dark['background-alt'],
       border: `solid 1px ${THEMES.dark.border}`,
     });
@@ -74,7 +74,7 @@ describe('ChatItem renders', () => {
 
   test('render isCurrent=true, theme dark', () => {
     renderComponent(<ChatItem chat={chat} isCurrent={true} onClick={() => {}} />, 'dark');
-    expect(screen.getByTestId('chatItem-container')).toHaveStyle({
+    expect(screen.getByTestId('chatItem__container')).toHaveStyle({
       'background-color': THEMES.dark['background'],
       border: `solid 1px ${THEMES.dark.primary}`,
     });
@@ -83,8 +83,8 @@ describe('ChatItem renders', () => {
   test('onClick', () => {
     const handler = jest.fn();
     renderComponent(<ChatItem chat={chat} isCurrent={false} onClick={handler} />);
-    fireEvent.click(screen.getByTestId('chatItem-container'));
-    fireEvent.click(screen.getByTestId('chatItem-container'));
+    fireEvent.click(screen.getByTestId('chatItem__container'));
+    fireEvent.click(screen.getByTestId('chatItem__container'));
     expect(handler).toHaveBeenCalledTimes(2);
     expect(handler).toHaveBeenCalledWith(chat.id);
   });
---
+++ src/entities/Message/Message.test.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { cleanup, screen, render } from '@testing-library/react';
+import { Message } from '@entities/Message';
+import { Message as MessageType } from '@store/types';
+import { renderComponent } from '@test/utils';
+import { MESSAGE_SERVICE_TYPES } from '@const/common';
+import { THEMES } from '@styles/theme';
+import { formatDate, getUserTitle } from './utils';
+
+const message: MessageType = {
+  id: '8PwpNK37lsBNdbWlUcswm',
+  text: 'test message text',
+  fromId: 'y_ZAWNqKsDgRLKLDu2Qne',
+  fromName: 'test_user',
+  date: new Date('2024-07-14').valueOf(),
+  service: 0,
+  index: 0,
+};
+const serviceMessage = { ...message, service: MESSAGE_SERVICE_TYPES.CHAT_LEFT };
+
+describe('Message entity', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  test('utils', () => {
+    expect(getUserTitle(message)).toEqual('test_user(2Qne)');
+    expect(formatDate(message.date)).toEqual('07/14/2024');
+  });
+
+  test('renders content', () => {
+    render(<Message message={message} />);
+    expect(screen.getByTestId('message__main-container')).toMatchSnapshot();
+  });
+
+  test('renders content service message', () => {
+    render(<Message message={serviceMessage} />);
+    expect(screen.getByText('test_user left chat')).toBeInTheDocument();
+  });
+
+  test('render non service, theme light', () => {
+    renderComponent(<Message message={message} />, 'light');
+    expect(screen.getByTestId('message__main-container-text')).toHaveStyle({
+      color: THEMES.light['text'],
+    });
+  });
+
+  test('render non service, theme dark', () => {
+    renderComponent(<Message message={message} />, 'dark');
+    expect(screen.getByTestId('message__main-container-text')).toHaveStyle({
+      color: THEMES.dark['text'],
+    });
+  });
+});
